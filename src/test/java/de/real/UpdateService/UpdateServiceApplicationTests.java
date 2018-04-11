575eb8d87c7dacb536364fc62f8a8a9b08c7f737
Tanzeem <tanzeemmd.zillu@realdigital.de>
2018-04-11 11:37:17 +0200

    Comple code

---
+++ src/test/java/de/real/UpdateService/UpdateServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package de.real.UpdateService;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UpdateServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
