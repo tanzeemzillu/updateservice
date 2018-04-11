454ae77a74052752f8bba0b7a9cf859dac57bdff
Tanzeem <tanzeemmd.zillu@realdigital.de>
2018-04-11 13:54:26 +0200

    Retrun book list as json object

--- src/main/java/de/real/UpdateService/topic/TopicController.java
+++ src/main/java/de/real/UpdateService/topic/TopicController.java
@@ -3,11 +3,18 @@ package de.real.UpdateService.topic;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+
 @RestController
 public class TopicController {
 
     @RequestMapping("/Topics")
-    public String getAllTopics(){
-        return"All Topics";
+    public List<Topic> getAllTopics(){
+        return Arrays.asList(
+                new Topic("book 1", "book name", "book description"),
+                new Topic("book 2", "book name", "book description"),
+                new Topic("book 3", "book name", "book description")
+        );
     }
 }
