36d0d8dab7430f19f9b7a82a4bb9402b6b50f86a
Tanzeem <tanzeemmd.zillu@realdigital.de>
2018-04-12 17:25:42 +0200

    introduce post method

--- src/main/java/de/real/UpdateService/topic/TopicController.java
+++ src/main/java/de/real/UpdateService/topic/TopicController.java
@@ -1,9 +1,7 @@
 package de.real.UpdateService.topic;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -22,5 +20,11 @@ public class TopicController {
     @RequestMapping("/Topics/{id}")
     public Topic getTopic(@PathVariable String id){
         return topicService.getTopic(id);
+    }
+
+    @RequestMapping(method = RequestMethod.POST , value = "/Topics")
+    public void addTopic(@RequestBody Topic topic){
+        topicService.addTopic(topic);
+
     }
 }
--- src/main/java/de/real/UpdateService/topic/TopicService.java
+++ src/main/java/de/real/UpdateService/topic/TopicService.java
@@ -2,18 +2,19 @@ package de.real.UpdateService.topic;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class TopicService {
 
-    private List<Topic> topics= Arrays.asList(
+    private List<Topic> topics= new ArrayList<>(Arrays.asList(
                 new Topic("book_1", "book name", "book description"),
                 new Topic("book_2", "book name", "book description"),
                 new Topic("book_3", "book name", "book description"),
                 new Topic("book_4", "book name", "book description")
-        );
+        ));
 
     public List<Topic> getAllTopics() {
         return topics;
@@ -21,5 +22,9 @@ public class TopicService {
 
     public Topic getTopic(String id){
         return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+    }
+
+    public void addTopic(Topic topic){
+        topics.add(topic);
     }
 }
